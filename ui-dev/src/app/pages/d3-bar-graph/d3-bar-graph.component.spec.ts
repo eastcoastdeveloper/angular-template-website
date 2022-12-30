58dcbc4a317592a38e80f152afc04e7a1dd09f64
eastcoastdeveloper <uxdeveloper@protonmail.com>
2022-12-30 10:05:56 -0800

    d3 & html-table cmpts

---
+++ ui-dev/src/app/pages/d3-bar-graph/d3-bar-graph.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { D3BarGraphComponent } from './d3-bar-graph.component';
+
+describe('D3BarGraphComponent', () => {
+  let component: D3BarGraphComponent;
+  let fixture: ComponentFixture<D3BarGraphComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ D3BarGraphComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(D3BarGraphComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ ui-dev/src/app/pages/html-table/html-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-html-table',
+  templateUrl: './html-table.component.html',
+  styleUrls: ['./html-table.component.scss']
+})
+export class HtmlTableComponent {
+
+}
