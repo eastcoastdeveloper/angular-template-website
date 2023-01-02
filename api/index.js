a770781f090f9284f3f34edc95c987de2350c326
eastcoastdeveloper <uxdeveloper@protonmail.com>
2023-01-01 18:13:15 -0800

    slider api

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ app.use('/api/accordion', require('./routes/accordion'));
 app.use('/api/dynamic-sidebar', require('./routes/dynamic-sidebar'));
 app.use('/api/table-paginated', require('./routes/table-paginated'));
 app.use('/api/code-samples', require('./routes/code-samples'));
+app.use('/api/slider', require('./routes/slider'));
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
---
+++ api/routes/slider.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const projectData = require('../json/slider.json');
+
+function sliderComponent(model) {
+  return (req, res, next) => {
+    results = model.sliderData;
+    res.sliderComponent = results;
+    next();
+  }
+}
+
+router.get('/', sliderComponent(projectData), (req, res) => {
+  res.json(res.sliderComponent);
+})
+
+module.exports = router;
