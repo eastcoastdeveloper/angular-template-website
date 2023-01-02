6cfbe97bbc921d1acef8fe635ac67d8906eb192b
eastcoastdeveloper <uxdeveloper@protonmail.com>
2023-01-01 17:59:19 -0800

    html table Angular page

--- ui-dev/src/app/development/components/html-table/html-table.component.ts
+++ ui-dev/src/app/development/components/html-table/html-table.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageDataObject } from 'src/app/interfaces/pageDataInterface';
+import { ProjectListService } from 'src/app/services/project-list.service';
 
 @Component({
   selector: 'app-html-table',
   templateUrl: './html-table.component.html',
   styleUrls: ['./html-table.component.scss']
 })
-export class HtmlTableComponent {
+export class HtmlTableComponent implements OnInit {
+  pageDataObject: PageDataObject = {
+    title: 'Table in HTML',
+    publishedOn: 'Oct 1, 2022',
+    updatedOn: 'Jan 3, 2023',
+    repoTitle: 'angular-date-picker',
+    repoLink:
+      'https://github.com/eastcoastdeveloper/datepicker-angular-component',
+    category: '',
+    views: 28,
+    forks: 0
+  };
 
+  constructor(private _projectListService: ProjectListService) {}
+
+  ngOnInit() {
+    // Send Page Data to Service & Wrapper
+    this._projectListService.changePageDataObject(this.pageDataObject);
+  }
 }
