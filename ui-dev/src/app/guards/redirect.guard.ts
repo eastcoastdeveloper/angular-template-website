744fe501ae1b5dfae99657080e8e7ae81a99a806
eastcoastdeveloper <uxdeveloper@protonmail.com>
2023-05-07 17:17:02 -0700

    update angular cli

--- ui-dev/src/app/guards/redirect.guard.ts
+++ ui-dev/src/app/guards/redirect.guard.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-  UrlTree
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class RedirectGuard implements CanActivate {
+export class RedirectGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
