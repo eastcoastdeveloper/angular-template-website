3a574d8995e435a39820d3a1be5c7972754441a6
eastcoastdeveloper <uxdeveloper@protonmail.com>
2024-02-04 12:45:17 -0800

    refine api

--- api/routes/compliance-library.js
+++ api/routes/compliance-library.js
@@ -2,52 +2,38 @@ const express = require("express");
 const router = express.Router();
 const projectData = require("../json/projects.json");
 
-function paginatedResults(model) {
-  return (req, res, next) => {
-    const results = {};
-    let filteredItems = [];
+router.get("/", (req, res) => {
+  const results = {};
+  let filteredItems = [];
 
-    const type = req.query.type;
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+  const type = req.query.type;
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
 
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+  const allCount = projectData.data.length;
+  const projectCount = projectData.data.filter((item) => item.category === "leadership").length;
+  const cmptCount = projectData.data.filter((item) => item.category === "standards").length;
+  const devCount = projectData.data.filter((item) => item.category === "security").length;
 
-    const allCount = model.data.length;
-    const projectCount = model.data.filter((item) => item.category === "leadership").length;
-    const cmptCount = model.data.filter((item) => item.category === "standards").length;
-    const devCount = model.data.filter((item) => item.category === "security").length;
+  if (type === "all") {
+    filteredItems = projectData.data;
+  } else {
+    filteredItems = projectData.data.filter((item) => {
+      return item.category === type;
+    });
+  }
 
-    if (type === "all") filteredItems = model.data;
-    if (type === "leadership")
-      filteredItems = model.data.filter((item) => {
-        return item.category === "leadership";
-      });
-    if (type === "standards")
-      filteredItems = model.data.filter((item) => {
-        return item.category === "standards";
-      });
-    if (type === "security")
-      filteredItems = model.data.filter((item) => {
-        return item.category === "security";
-      });
+  results[type] = filteredItems.slice(startIndex, endIndex);
 
-    results[type] = filteredItems.slice(startIndex, endIndex);
-
-    results.totals = {
-      all: allCount,
-      leadership: projectCount,
-      standards: cmptCount,
-      security: devCount,
-    };
-    res.paginatedResults = results;
-    next();
+  results.totals = {
+    all: allCount,
+    leadership: projectCount,
+    standards: cmptCount,
+    security: devCount,
   };
-}
-
-router.get("/", paginatedResults(projectData), (req, res) => {
-  res.json(res.paginatedResults);
+  res.json(results);
 });
 
 module.exports = router;
